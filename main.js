e977e4984bcbaebb578501866f3fb92839aeb1e1
Selfmade20e <tebogo1686@gmail.com>
2020-09-01 11:43:43 +0200

    added the changes i made on my app

---
+++ main.js
@@ -0,0 +1,19 @@
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+    var file = "http://www.yoursite.com/somefile.png";
+    var randomNum = Math.round(Math.random() * 10000);
+
+    xhr.open('HEAD', file + "?rand=" + randomNum, false);
+
+    try {
+        xhr.send();
+
+        if (xhr.status >= 200 && xhr.status < 304) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+}
